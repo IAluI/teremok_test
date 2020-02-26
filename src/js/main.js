474bec637de5246bdadd2852a273407f85096cfb
IAluI <29124424+ialui@users.noreply.github.com>
2020-02-27 05:28:53 +0700

    -Меню, банеры

--- src/js/main.js
+++ src/js/main.js
@@ -31,17 +31,233 @@ $(document).ready(() => {
     }
   });
 
+  let desToMobBp = window.matchMedia('(min-width: 1280px)');
+
+  function swiperResponsive(options) {
+    let breakpoint = window.matchMedia(options.mediaQ);
+    let mySwiper;
+
+    function breakpointChecker() {
+      if (breakpoint.matches === true) {
+        if (mySwiper !== undefined) {
+          mySwiper.destroy(true, true);
+        }
+        if (options.desctopOptions) {
+          mySwiper = new Swiper(options.swiperContainer, options.desctopOptions);
+        }
+      } else {
+        if (mySwiper !== undefined) {
+          mySwiper.destroy(true, true);
+        }
+        if (options.mobileOptions) {
+          mySwiper = new Swiper(options.swiperContainer, options.mobileOptions);
+        }
+      }
+    }
+
+    breakpoint.addListener(breakpointChecker);
+    breakpointChecker();
+
+    return mySwiper;
+  }
+
+  /*
+  Продуктовый слайдер десктоп
+   */
+  $('.sec-food-menu__desctop-menu .swiper-container').each((index, item) => {
+    swiperResponsive({
+      mediaQ: '(min-width: 1280px)',
+      swiperContainer: item,
+      desctopOptions : {
+        speed: 600,
+        slidesPerView: 'auto',
+        centeredSlides: true,
+        loopedSlides: 10,
+        loop: true,
+        effect: 'fade',
+        fadeEffect: {
+          crossFade: true
+        },
+        autoplay: {
+          delay: 3000,
+        },
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+          type: 'bullets',
+        },
+      },
+      mobileOptions: false
+    });
+  });
+  /*
+   Продуктовый слайдер мобилка
+   */
+  function prodSliderMobile() {
+    if (desToMobBp.matches === false) {
+      let mobMenuNavLine = $('.sec-food-menu__mobile-menu-line');
+      let mobMenuNav = swiperResponsive({
+        mediaQ: '(min-width: 1280px)',
+        swiperContainer: '.sec-food-menu__mobile-menu-nav',
+        desctopOptions : false,
+        mobileOptions: {
+          slidesPerView: 'auto',
+          centeredSlides: true,
+          spaceBetween: 20,
+          navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+          },
+          on: {
+            init: function () {
+              mobMenuNavLine.width(this.slides[0].offsetWidth);
+            },
+            slideChange: function () {
+              mobMenuNavLine.width(this.slides[this.activeIndex].offsetWidth);
+            },
+            tap: function () {
+              if (this.clickedIndex !== undefined) {
+                this.slideTo(this.clickedIndex);
+              }
+            },
+          }
+        }
+      });
+
+      let mobMenuSlider = swiperResponsive({
+        mediaQ: '(min-width: 1280px)',
+        swiperContainer: '.sec-food-menu__mobile-menu-slider',
+        desctopOptions : false,
+        mobileOptions: {
+          slidesPerView: 'auto',
+        }
+      });
+
+      let menuSections = [];
+      let menuSectionsIndexes = [];
+      $('.sec-food-menu__mobile-menu-nav > .swiper-wrapper > .swiper-slide').each((index, item) => {
+        menuSections.push($(item).attr('data-menuSection'));
+      });
+      for (let j = 0; j < menuSections.length; j++) {
+        for (let i = 0; i < mobMenuSlider.slides.length; i++) {
+          if ($(mobMenuSlider.slides[i]).find('.sec-food-menu__menu-item--' + menuSections[j]).length !== 0) {
+            menuSectionsIndexes.push(i);
+            break;
+          }
+        }
+      }
+
+      let crt = true;
+      mobMenuNav.on('slideChange', function () {
+        if (crt) {
+          mobMenuSlider.slideTo(menuSectionsIndexes[this.activeIndex]);
+        } else {
+          crt = true;
+        }
+      });
+
+      mobMenuSlider.on('slideChange', function () {
+        let numOfSec = menuSectionsIndexes.length;
+        for (let i = 0; i < numOfSec; i++) {
+          if (menuSectionsIndexes[i] > this.activeIndex) {
+            if (mobMenuNav.activeIndex !== i - 1) {
+              crt = false;
+              mobMenuNav.slideTo(i - 1);
+            }
+            return;
+          }
+        }
+        if (menuSectionsIndexes[numOfSec - 1] <= this.activeIndex && mobMenuNav.activeIndex !== numOfSec - 1) {
+          mobMenuNav.slideTo(numOfSec - 1);
+          return;
+        }
+      });
+    }
+  }
+  prodSliderMobile();
+  desToMobBp.addListener(prodSliderMobile);
+
+  let bannerSlider = swiperResponsive({
+    mediaQ: '(min-width: 1280px)',
+    swiperContainer: '.sec-food-menu__banners',
+    desctopOptions : {
+      speed: 600,
+      slidesPerView: 'auto',
+      centeredSlides: true,
+      loopedSlides: 10,
+      loop: true,
+      effect: 'fade',
+      fadeEffect: {
+        crossFade: true
+      },
+      autoplay: {
+        delay: 3000,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        type: 'bullets',
+      },
+    },
+    mobileOptions: {
+      slidesPerView: 'auto',
+      centeredSlides: true,
+      loopedSlides: 10,
+      loop: true,
+    }
+  });
+
+  let secMenu = $('.sec-food-menu');
+  let secNews = $('.sec-news');
+  let secWrapper = $('.sec-news__wrapper');
+  let scrollSpy = debounce(function() {
+    let scrolled = jqWindow.scrollTop();
+    let secMenuPos = secMenu.offset().top - jqWindow.height();
+    if (scrolled > secMenu.offset().top && scrolled < secMenuPos + secMenu.height()) {
+      secWrapper.scrollTop((secNews.height() - secMenu.height()) / (secNews.height() - jqWindow.height()) * (scrolled - secMenuPos - jqWindow.height()));
+    } else if (scrolled < secMenu.offset().top) {
+      secWrapper.scrollTop(0);
+    } else if (scrolled > secMenuPos + secMenu.height()) {
+      secWrapper.scrollTop(secNews.height() - secMenu.height());
+    }
+  },
+  100,
+  {
+    maxWait: 100,
+  });
+  function initAscycScroll() {
+    if (desToMobBp.matches === true) {
+      setTimeout(function () {
+        if (secNews.height() < secMenu.height()) {
+          return;
+        }
+        jqWindow.on('scroll', scrollSpy);
+        secWrapper.height(secMenu.height());
+      }, 2000);
+    } else {
+      secWrapper.scrollTop(0);
+      secWrapper.height('');
+      jqWindow.off('scroll', scrollSpy);
+    }
+  }
+  initAscycScroll();
+  desToMobBp.addListener(initAscycScroll);
+
   let partnersSlider = new Swiper('.sec-partners__partners-slider', {
     slidesPerView: 'auto',
     freeMode: true,
     loopedSlides: 10,
     loop: true,
+    longSwipes: false,
+    slidesPerGroup: 7,
   });
   let suppliersSlider = new Swiper('.sec-partners__suppliers-slider', {
     slidesPerView: 'auto',
     freeMode: true,
     loopedSlides: 10,
     loop: true,
+    longSwipes: false,
+    slidesPerGroup: 7,
   });
   let slidersWrapper = new Swiper('.sec-partners__sliders-wrapper', {
     slidesPerView: 1,
